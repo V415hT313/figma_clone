c904e5c7bd8d0a6f3905af10555f5d854f52519b
V415hT313 <vashu2506rkt@gmail.com>
2024-02-05 23:36:24 +0530

    ---

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import dynamic from "next/dynamic";
 
+/**
+ * disable ssr to avoid pre-rendering issues of Next.js
+ *
+ * we're doing this because we're using a canvas element that can't be pre-rendered by Next.js on the server
+ */
+const App = dynamic(() => import("./App"), { ssr: false });
 
-export default function Page() {
-  return (
-    <h1 className="text-5xl text-white">Hello</h1>
-  );
-}
+export default App;
